59c25b677d9df7f28cfea054ce1adb7d1842cb08
Bhargav Karanam <bhargav.karanam@gmail.com>
2017-09-23 20:59:50 +0530

    Registration module.

--- js/script.js
+++ js/script.js
@@ -22,14 +22,38 @@ $("#login").on('click', function(ev){
 	let password = $("#password").val();
 
 	firebase.auth().signInWithEmailAndPassword(email, password).then(function(status){
-		console.log(status);
+		toastr.success('Successful!');
 	})
 	.catch(function(error) {
 	  // Handle Errors here.
-	  console.log(error);
+	  
 	  var errorCode = error.code;
 	  var errorMessage = error.message;
-	  // ...
+	  toastr.error(errorMessage);
 	});
 })
 
+$("#register").on('click', function(ev){
+	ev.preventDefault();
+	let email = $("#emailReg").val();
+	let password = $("#passReg").val();
+	let confirm = $("#confirmPassReg").val();
+
+	console.log(email + password + confirm);
+
+	if(password === confirm)
+	{
+		firebase.auth().createUserWithEmailAndPassword(email, password)
+		.then(function(user){
+			toastr.success('Registered successfully!');
+		})
+		.catch(function(error) {
+  
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+  		  toastr.error(errorMessage);
+		});
+	}
+	else
+		toastr.error('Passwords do not match.');
+});
